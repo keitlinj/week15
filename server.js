2925b11a555d2bcded47674e53ed6f592b4f7a83
Keitlin Jalast <keitlin.jalast@tktk.ee>
2025-09-30 15:50:05 +0300

    add week 15 files

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (request, response) => {
+
+    const jsonFilePath = path.join(__dirname, 'data', 'cvData.json');
+
+    fs.readFile(jsonFilePath, (error, dataFromFile) => {
+        if(error) {
+            console.error('Failed to read from file', error);
+            return response.status(500).send('Failed to read from file.');
+        }
+
+        const jsonData = JSON.parse(dataFromFile);
+        response.render('index');
+
+        //console.log(jsonData);
+        //response.json(JSON.parse(dataFromFile));
+
+    })
+});
+
+
+
+app.listen(PORT, () => {
+    console.log('Server is running on port: ' + PORT);
+})
